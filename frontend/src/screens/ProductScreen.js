30a475b08cfe97e72e150ba92c05f362985db522
Jakub Widurek <jakubwidurek@gmail.com>
2021-11-10 19:23:32 +0100

    create ProductScreen component

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,76 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  ListGroupItem,
+} from 'react-bootstrap';
+import Rating from '../components/Rating';
+import products from '../products';
+import { useParams } from 'react-router';
 
 const ProductScreen = () => {
-  return <div>sfsdf</div>;
+  let { id } = useParams();
+  const product = products.find((p) => p._id === id);
+
+  return (
+    <>
+      <Link className='btn btn-light my-3' to='/'>
+        Go Back
+      </Link>
+      <Row>
+        <Col sm={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col sm={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h5>{product.name}</h5>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: ${product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status</Col>
+                  <Col>
+                    {product.countInStock > 0 ? 'InStoctk' : 'Out of Stock'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item className='d-grid gap-2'>
+                <Button type='button' disabled={product.countInStock === 0}>
+                  Add To Card
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default ProductScreen;
