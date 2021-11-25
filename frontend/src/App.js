08062538959abc50ef03ec1cd929ee0ba162fa16
Jakub Widurek <jakubwidurek@gmail.com>
2021-11-25 18:06:11 +0100

    create user login screen and functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 
 function App() {
   return (
@@ -15,10 +16,11 @@ function App() {
         <Container>
           <h1>Welcome to E-commerce App.</h1>
           <Routes>
-            <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/login' element={<LoginScreen />} />
             <Route path='/product/:id' element={<ProductScreen />} />
             <Route path='/cart' element={<CartScreen />} />
             <Route path='/cart/:id' element={<CartScreen />} />
+            <Route path='/' element={<HomeScreen />} exact />
           </Routes>
         </Container>
       </main>
