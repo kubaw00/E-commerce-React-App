9567a7348f584c64c2f2b9d00d0c77431cc4e6ce
Jakub Widurek <jakubwidurek@gmail.com>
2021-11-28 11:22:11 +0100

    update profile endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -59,6 +59,34 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc Update user profile
+// @route PUT /api/users/profile
+// @access Private
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+    res.status(201).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
 // @desc Get user profile
 // @route POST /api/users/profile
 // @access Private
@@ -79,4 +107,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { registerUser, authUser, getUserProfile };
+export { registerUser, authUser, getUserProfile, updateUserProfile };
