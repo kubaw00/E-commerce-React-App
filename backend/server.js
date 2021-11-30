db85f720c9f9d39d3913d05d9a3f0c38b3b262b5
Jakub Widurek <jakubwidurek@gmail.com>
2021-11-30 19:35:57 +0100

    create order

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes);
+app.use('/api/order', orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
