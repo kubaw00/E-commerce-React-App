3623e36dbf80035e444173278c666a6ed5086f93
Jakub Widurek <jakubwidurek@gmail.com>
2021-12-09 22:58:33 +0100

    add check for order

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -16,8 +16,10 @@ const OrderScreen = () => {
   const { order, loading, error } = orderDetails;
 
   useEffect(() => {
-    dispatch(getOrderDetails(orderId));
-  }, [dispatch, orderId]);
+    if (!order || order._id !== orderId) {
+      dispatch(getOrderDetails(orderId));
+    }
+  }, [order, orderId, dispatch]);
 
   return loading ? (
     <Loader />
