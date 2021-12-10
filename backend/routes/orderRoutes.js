c0243e5486012898d98dcb0cf48b8a3fde9b0f9d
Jakub Widurek <jakubwidurek@gmail.com>
2021-12-10 19:31:43 +0100

    add order list reducer and

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,12 +3,14 @@ import {
   getOrderItems,
   getOrderById,
   updateOrderToPaid,
+  getMyOrders,
 } from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.route('/').post(protect, getOrderItems);
+router.route('/myorders').get(protect, getMyOrders);
 router.route('/:id').get(protect, getOrderById);
 router.route('/:id/pay').put(protect, updateOrderToPaid);
 
