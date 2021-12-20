d3178e565238e53b386acdf4f1759508747ef21a
Jakub Widurek <jakubwidurek@gmail.com>
2021-12-20 23:17:03 +0100

    create top product rated reducer, action and  constants

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -146,6 +146,16 @@ const createProductReview = asyncHandler(async (req, res) => {
   }
 });
 
+//@desc Get top rated procucts
+//@route POST /api/products/top
+//@access Public
+
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+
+  res.json(products);
+});
+
 export {
   getProductById,
   getProducts,
@@ -153,4 +163,5 @@ export {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 };
